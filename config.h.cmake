1410e19259e5df73d7b795129b99e3bd9ff5e50c
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2012-04-07 18:39:25 -0400

    Adopting CMake build system

---
+++ config.h.cmake
@@ -0,0 +1,16 @@
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define to 1 if the X Window System is missing or not being used. */
+#cmakedefine X_DISPLAY_MISSING
+
+
+#define PACKAGE "@PACKAGE_NAME@"
+#define PACKAGE_NAME "@PACKAGE_NAME@"
+#define PACKAGE_BUGREPORT "@PACKAGE_BUGREPORT@"
+#define PACKAGE_VERSION "@PACKAGE_VERSION@"
+#define PACKAGE_STRING "@PACKAGE_STRING@"
+#define VERSION PACKAGE_VERSION
+
+
